d9b06324bce977cd6237be25b26312bcf111ab44
Santiago Bayona <santiago.bayona@mail.escuelaing.edu.co>
2023-09-24 22:42:43 -0500

    first commit

---
+++ src/test/java/org/adaschool/Weather/WeatherReportServiceTest.java
@@ -0,0 +1,48 @@
+package org.adaschool.Weather;
+
+import org.adaschool.Weather.controller.WeatherReportController;
+import org.adaschool.Weather.data.WeatherApiResponse;
+import org.adaschool.Weather.data.WeatherReport;
+import org.adaschool.Weather.service.WeatherReportService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+class WeatherReportServiceTest {
+
+    private WeatherReportController wrc;
+
+    private WeatherReportService wrs;
+
+    @Test
+    public void WhenISendARequestShouldGiveMeAWeatherData() {
+        RestTemplate restTemplate = new RestTemplate();
+        wrc = Mockito.mock(WeatherReportController.class);
+        String url =  "https://api.openweathermap.org/data/2.5/weather"+  "?lat=" + 44.34 + "&lon=" + 10.99 + "&appid=" + "02d85f539125fb0a3600f2b096082230";
+        WeatherApiResponse response = restTemplate.getForObject(url, WeatherApiResponse.class);
+        WeatherReport report = new WeatherReport();
+        WeatherApiResponse.Main main = response.getMain();
+        report.setTemperature(main.getTemperature());
+        report.setHumidity(main.getHumidity());
+        Mockito.when(wrc.getWeatherReport(44.34,10.99)).thenReturn(report);
+        WeatherReport wr = wrc.getWeatherReport(44.34,10.99);
+        Mockito.verify(wrc).getWeatherReport(44.34,10.99);
+    }
+
+    @Test
+    public void WhenIUseWeatherReportServiceShouldGiveMeAWeatherData(){
+        wrs = Mockito.mock(WeatherReportService.class);
+        RestTemplate restTemplate = new RestTemplate();
+        String url =  "https://api.openweathermap.org/data/2.5/weather"+  "?lat=" + 44.34 + "&lon=" + 10.99 + "&appid=" + "02d85f539125fb0a3600f2b096082230";
+        WeatherApiResponse response = restTemplate.getForObject(url, WeatherApiResponse.class);
+        WeatherReport report = new WeatherReport();
+        WeatherApiResponse.Main main = response.getMain();
+        report.setTemperature(main.getTemperature());
+        report.setHumidity(main.getHumidity());
+        Mockito.when(wrs.getWeatherReport(44.34,10.99)).thenReturn(report);
+        WeatherReport wr = wrs.getWeatherReport(44.34,10.99);
+        Mockito.verify(wrs).getWeatherReport(44.34,10.99);
+    }
+}
